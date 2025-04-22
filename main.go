ca296831d9bda8fe20635488f9726a6409a6e996
Brooke Bachman <bbachman1994@gmail.com>
2025-04-22 17:13:21 -0600

    rename route

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 func main() {
     log.Println("Starting server on :8080")
 
-    http.HandleFunc("/ingest", IngestHandler)
+    http.HandleFunc("/login", CreateLoginEvent)
     http.HandleFunc("/suspicious", SuspiciousHandler)
 
     log.Fatal(http.ListenAndServe(":8080", nil))
