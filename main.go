bcf8438aed39aae1432d42f0623f965161891ce6
Brooke Bachman <bbachman1994@gmail.com>
2025-04-22 18:10:16 -0600

    configure cors so index.html can run;

--- main.go
+++ main.go
@@ -3,14 +3,26 @@ package main
 import (
 	"log"
 	"net/http"
+
+	"github.com/rs/cors"
 )
 
 func main() {
     log.Println("Starting server on :8080")
 
+    // Add CORS support
+    c := cors.New(cors.Options{
+        AllowedOrigins: []string{"http://localhost:8000"}, // or "*" to allow all origins
+        AllowedMethods: []string{"GET", "POST", "PUT", "DELETE"},
+        AllowedHeaders: []string{"Content-Type"},
+    })
+
+    // Apply CORS middleware
     http.HandleFunc("/login-events", CreateLoginEvent)
     http.HandleFunc("/suspicious", SuspiciousHandler)
 
-    log.Fatal(http.ListenAndServe(":8080", nil))
+    handler := c.Handler(http.DefaultServeMux)
+
+    log.Fatal(http.ListenAndServe(":8080", handler))
 }
 
